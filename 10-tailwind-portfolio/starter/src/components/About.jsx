c1039597b2ca1895a2301b20267dbd6aa59048fd
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2025-03-29 08:49:13 +0400

    Fixes

--- 10-tailwind-portfolio/starter/src/components/About.jsx
+++ 10-tailwind-portfolio/starter/src/components/About.jsx
@@ -2,8 +2,8 @@ import aboutSvg from "../assets/about.svg";
 import SectionTitle from "./SectionTitle";
 const About = () => {
   return (
-    <section class="bg-white py-20" id="about">
-      <div class="align-element grid  md:grid-cols-2 items-center gap-16">
+    <section className="bg-white py-20" id="about">
+      <div className="align-element grid  md:grid-cols-2 items-center gap-16">
         <img src={aboutSvg} className="w-full h-64" />
         <article>
           <SectionTitle title="code and coffee" />
